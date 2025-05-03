5ba67588b5cf630d3d052d186d56a365737c952d
Kendra Dias <kendra.dias@hotmail.com>
2025-05-03 14:21:22 -0700

    removed test folder again, it somehow got re added

--- tests/webhookRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express';
-//import { handleWebhook } from '../controllers/webhookController';
-import { handleWebhook } from './webhookController';
-
-const router: Router = express.Router();
-
-router.post('/docusign-webhook', handleWebhook);
-
-export default router;
