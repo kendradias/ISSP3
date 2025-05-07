f1ce11622b2490f358d1e55f56fc262668731179
Sangita1992 <panwarsangeeta04@gmail.com>
2025-05-07 10:26:56 -0700

    update Cron function

--- backend-app/src/utils/cron.ts
+++ backend-app/src/utils/cron.ts
@@ -1,10 +1,26 @@
 import cron from 'node-cron';
 import { recoverMissedEnvelopes } from '../services/docuSignService.ts';
 
+let isRunning = false;
+
 export function startCronJobs(): void {
-  cron.schedule('*/1 * * * *', () => {     //start job in every 60 seconds or 1 minute
-    console.log('Running recovery cron job...');
-    recoverMissedEnvelopes();
+  cron.schedule('*/1 * * * *', async () => {
+    if (isRunning) {
+      console.log('Previous recovery job still running, skipping this round.');
+      return;
+    }
+
+    console.log('Starting recovery cron job...');
+    isRunning = true;
+
+    try {
+      await recoverMissedEnvelopes();
+    } catch (error) {
+      console.error('Error during recovery:', error);
+    } finally {
+      isRunning = false;
+      console.log('Recovery cron job completed.');
+    }
   });
 }
 
